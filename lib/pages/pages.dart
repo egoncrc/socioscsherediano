91c5b659572d05ad8af9db79ec440a367d8d0531
egoncrc <52466120+egoncrc@users.noreply.github.com>
2023-03-22 10:53:01 -0600

    Update pages.dart

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,7 +1,3 @@
-
-
-
-
 export 'package:socioscsherediano/pages/home_page.dart';
 export 'package:socioscsherediano/pages/login_page.dart';
 export 'package:socioscsherediano/pages/partidos_page.dart';
