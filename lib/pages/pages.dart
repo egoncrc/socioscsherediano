b974b383bf68f2a3d555234f50f470976b98875a
egoncrc <egoncrc@gmail.com>
2023-03-22 10:51:42 -0600

    SE ELIMINA PAGINAS DE RETO

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -5,5 +5,4 @@
 export 'package:socioscsherediano/pages/home_page.dart';
 export 'package:socioscsherediano/pages/login_page.dart';
 export 'package:socioscsherediano/pages/partidos_page.dart';
-export 'package:socioscsherediano/pages/reto_page.dart';
-export 'package:socioscsherediano/pages/retos_page.dart';
+
